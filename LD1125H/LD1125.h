26a06ceb7e7e18418a52629f9bcd9226ecb93449
patrick3399 <99384328+patrick3399@users.noreply.github.com>
2022-06-17 10:48:06 +0800

    Update LD1125.h

--- LD1125H/LD1125.h
+++ LD1125H/LD1125.h
@@ -1,19 +1,44 @@
 #include "esphome.h"
-class LD1125UART : public Component, public UARTDevice, public TextSensor {
+class LD1125H : public Component, public UARTDevice, public TextSensor {
  public:
-  UartReadLineSensor(UARTComponent *parent) : UARTDevice(parent) {}
+  LD1125H(UARTComponent *parent) : UARTDevice(parent) {}
   Sensor *status_sensor = new Sensor();
   Sensor *distance_sensor = new Sensor();
 
   void setup() override {
     // nothing to do here
   }
-  void loop() override {
-    String line = readString();
-    int i = parseInt();
-    while (available()) {
-      char c = read();
-    }
+  int readline(int readch, char *buffer, int len)
+  {
+    static int pos = 0;
+    int rpos;
 
+    if (readch > 0) {
+      switch (readch) {
+        case '\n': // Ignore new-lines
+          break;
+        case '\r': // Return on CR
+          rpos = pos;
+          pos = 0;  // Reset position index ready for next time
+          return rpos;
+        default:
+          if (pos < len-1) {
+            buffer[pos++] = readch;
+            buffer[pos] = 0;
+          }
+      }
+    }
+    // No end of line has been found, so return -1.
+    return -1;
+  }
+
+  void loop() override {
+    const int max_line_length = 80;
+    static char buffer[max_line_length];
+    while (available()) {
+      if(readline(read(), buffer, max_line_length) > 0) {
+        publish_state(buffer);
+      }
+    }
   }
 };
